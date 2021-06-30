5073d7fc7c895452887f454f1827459388c5fe29
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-06-30 17:13:27 +0200

    Add first commponents and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 const App = () => {
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+
+      </div>
     </div>
   )
 }
