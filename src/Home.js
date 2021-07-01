7d4f14850251380f22d0f97f8d203a00cdcd4dc2
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-07-01 21:49:07 +0200

    Add list blog component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3 },
+        { title: 'The end of all', body: 'lorem ipsum...', author: 'Artur', id: 4 }
+
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            <BlogList blogs={blogs} title="All Blogs:" />
         </div>
     );
 }
