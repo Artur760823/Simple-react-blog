bfe0e0d267db0f74564509af88fafea8c48ac8ef
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-07-05 17:21:08 +0200

    Add delete button

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,20 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
         { title: 'The end of all', body: 'lorem ipsum...', author: 'Artur', id: 4 }
 
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs:" />
+            <BlogList blogs={blogs} title="All Blogs:" handleDelete={handleDelete} />
+
         </div>
     );
 }
