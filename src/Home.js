4c7430daa57e96db43664604f1216cf29d289978
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-07-15 19:45:06 +0200

    Add adding new blog and custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+    const { data: blogs, setData, isPending, error } = useFetch('http://localhost:8000/blogs');
 
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
+        setData(newBlogs);
     }
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-            });
-    }, [])
+
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs:" handleDelete={handleDelete} />}
         </div>
     );
